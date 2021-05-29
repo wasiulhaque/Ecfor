5d9dcd76184c1331609f761b23c684d1fff0d153
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-05-29 11:39:57 +0600

    database intiated

--- Communication/admin.py
+++ Communication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from django.contrib.admin.decorators import register
+from Communication.models import testDb
 # Register your models here.
+
+admin.site.register(testDb)
