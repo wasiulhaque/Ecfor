d97e0bdd777c69b2b25ce7f582330666ee21ef11
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-05-29 20:47:10 +0600

    text to speech feature added

--- Communication/views.py
+++ Communication/views.py
@@ -3,7 +3,12 @@ from django.http import HttpResponse
 
 from django.contrib.auth.decorators import login_required
 
+# speech to text
 import speech_recognition as sr
+
+# text to speech
+from gtts import gTTS
+import os
 
 # Create your views here.
 
@@ -26,7 +31,7 @@ def listen(request):
         try:
             text = r.recognize_google(audio)
             print("You said: {}".format(text))
-            text_file = open("speech_to_txt", "wt")
+            text_file = open("speech_to_text", "wt")
             text_file.write(text)
             text_file.close()
         except:
@@ -39,8 +44,30 @@ def listen(request):
 
 
 def read_file():
-    file = open('speech_to_txt', 'r')
+    file = open('speech_to_text', 'r')
     file_content = file.read()
     file.close()
     # file_content = "BD"
     return file_content
+
+
+@login_required
+def speak(request):
+
+    # will read the file to convert into audio
+    text_File = open("text_to_speech.txt", "r")
+    myText = text_File.read().replace("\n", " ")
+    # line ending er jaygay jate shesh na vabe instead break nibe bolar time a
+
+    language = 'en'
+
+    # slow means audio will played slow
+    output = gTTS(text=myText, lang=language, slow=False)
+
+    output.save("output.mp3")
+
+    text_File.close()
+
+    os.system("start output.mp3")
+
+    return redirect('Communication:index')
