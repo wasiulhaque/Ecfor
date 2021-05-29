abe7a555900e606d92c342012c33a0e69655cf7f
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-05-29 17:36:38 +0600

    login required added, Authentication completed

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
@@ -17,39 +18,45 @@ from Authentication.forms import CreateUserForm
 
 @csrf_exempt
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('Communication:index')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)  # render the form data
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(
-                request, 'Account is created successfully for ' + user)
-            return redirect('Authentication:login')
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)  # render the form data
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(
+                    request, 'Account is created successfully for ' + user)
+                return redirect('Authentication:login')
 
-    context = {'form': form}
-    return render(request, 'register.html', context=context)
+        context = {'form': form}
+        return render(request, 'register.html', context=context)
 
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('Communication:index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('Communication:index')
+            else:
+                messages.info(request, "Username or Password is incorrect.")
 
-        if user is not None:
-            login(request, user)
-            return redirect('Communication:index')
-        else:
-            messages.info(request, "Username or Password is incorrect.")
-
-    context = {}
-    return render(request, 'login.html', context)
+        context = {}
+        return render(request, 'login.html', context)
 
 
+@login_required(login_url='Authentication:login')
 def logoutUser(request):
     logout(request)
     return redirect('Authentication:login')
--- Communication/views.py
+++ Communication/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-@login_required
+@login_required(login_url='Authentication:login')
 def index(request):
     diction = {'sample_text': 'This is a text'}
     return render(request, 'index.html', context=diction)
