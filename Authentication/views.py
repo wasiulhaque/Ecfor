07769be1fa1fb797c84a8437454258e34165686f
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-05-29 13:55:03 +0600

    registration can be done

--- Authentication/views.py
+++ Authentication/views.py
@@ -1,12 +1,26 @@
+from django import forms
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.contrib.auth.forms import UserCreationForm
 
+
+from Authentication.forms import CreateUserForm
 # Create your views here.
 
 
+@csrf_exempt
 def registerPage(request):
-    context = {}
-    return render(request, 'register.html', context)
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)  # render the form data
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'register.html', context=context)
 
 
 def loginPage(request):
